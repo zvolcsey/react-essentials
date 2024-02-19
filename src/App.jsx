1f310c93c482b22cb35fb1d14f8c363673be5ea9
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-19 22:50:57 +0100

    feat: Added TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton/TabButton';
 
 function App() {
   return (
@@ -15,6 +16,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />            
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,9 @@
+export default function TabButton({children}) {
+  return (
+    <li>
+      <button>
+        {children}
+      </button>
+    </li>
+  );
+}
