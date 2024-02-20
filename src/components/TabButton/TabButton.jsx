403b7552f4e02c27298e6aebc0fae379d37ab98e
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-20 12:02:26 +0100

    feat: Added active class dynamically

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,7 +1,10 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
   return (
     <li>
-      <button onClick={onSelect} >
+      <button
+        className={isSelected ? "active" : undefined}
+        onClick={onSelect} 
+      >
         {children}
       </button>
     </li>
