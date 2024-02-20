424191fcaae9f3c6af5761c5d62e0d47b32038d8
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-20 10:39:23 +0100

    feat: Managed selectedTopic with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
   function handleSelect(selectedButton) {
     // selectedButton => 'jsx', 'state', 'components', 'props'
-    console.log(selectedButton)
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -30,7 +33,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')} >Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
           </menu>
-          
+          {selectedTopic}
         </section>
       </main>
     </div>
