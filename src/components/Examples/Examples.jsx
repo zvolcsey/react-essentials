cc5bd08a4c826fcbf800ecc85e5524765fff5495
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-21 11:56:27 +0100

    feat: Added Section component

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../../data";
 import TabButton from "../TabButton/TabButton";
+import Section from "../Section/Section";
 import "./Examples.css";
 
 export default function Examples() {
@@ -26,27 +27,26 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title={"Examples"} id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')} 
+          onClick={() => handleSelect('components')} 
         >Components</TabButton>
         <TabButton 
           isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')} 
+          onClick={() => handleSelect('jsx')} 
         >JSX</TabButton>
         <TabButton 
           isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')} 
+          onClick={() => handleSelect('props')} 
         >Props</TabButton>
         <TabButton
           isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')} 
+          onClick={() => handleSelect('state')} 
         >State</TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
